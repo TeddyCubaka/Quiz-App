ad220695a18ccea9dfb506f237b8b6390679562d
Teddy Cubaka <teddycubaka96@gmail.com>
2022-05-19 11:21:44 +0100

    event on the button of the first page, it's display the first questions

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//ce code ci dessous gère le miniteur.
+
+const departMinutes = 1;
+let temps = departMinutes * 60;
+
+const timerElement = document.getElementById("timer");
+
+setInterval(() => {
+  let minutes = parseInt(temps / 60, 10);
+  let secondes = parseInt(temps % 60, 10);
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  secondes = secondes < 10 ? "0" + secondes : secondes;
+
+  timerElement.innerText = `${minutes}:${secondes}`
+  temps = temps <= 0 ? 0 : temps - 1
+}, 1000);
+
+
+//cachons la première page et passons à l'autre
+
+let begginBtn = document.querySelector("#begginBtn");
+let header = document.querySelector("header");
+let questions = document.querySelector(".questions");
+begginBtn.addEventListener("click", function(beggin){
+    header.classList.add("hide");
+    questions.classList.remove("hide");
+});
+
